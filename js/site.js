e4ca833d6bbaf415557699482886969f48cc9c21
nthacks <nischal@nthacks.com>
2018-06-26 23:35:02 +0530

    Updated service worker

--- js/site.js
+++ js/site.js
@@ -1,1 +1,9 @@
-navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function (registration) {console.log('Excellent, registered with scope: ', registration.scope);});
+if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+    navigator.serviceWorker.register('./sw.js', {
+        scope: './'
+    }).then(function (reg) {
+        console.log('Service worker has been registered for scope:' + reg.scope);
+    });
+}
